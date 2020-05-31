6834bb3e0723a8c64daa54bad4d4b1d2a9c2ab23
mudl <codybaldwin@gmail.com>
2020-05-31 23:02:32 +0000

    JS cleanup

--- app/assets/javascripts/noshis.js
+++ app/assets/javascripts/noshis.js
@@ -1,13 +1,15 @@
 $( document ).on('turbolinks:load', function() {
   $(".noshi_type").change(function () {
-    if ($('#noshi_ntype_15').is( ':checked' ) || $('#noshi_ntype_16').is( ':checked' )) {
-      $('#noshi_namae4').prop('readonly', true)
-      $('#noshi_namae5').prop('readonly', true)
-      $('#noshi_namae4').val('')
-      $('#noshi_namae5').val('')
+    if ($('#noshi_ntype_15, #noshi_ntype_16, #noshi_ntype_17').is( ':checked' )) {
+      $('#noshi_namae2, #noshi_namae3, #noshi_namae4, #noshi_namae5').prop('readonly', true)
+      $('#noshi_namae4, #noshi_namae5').val('')
+      $("#noshi_namae").keyup(function () {
+        var repeat = $('#noshi_namae').val()
+        $('#noshi_namae2, #noshi_namae3').val(repeat)
+        $('#noshi_namae4, #noshi_namae5').val('')
+      });
     } else {
-      $('#noshi_namae4').prop('readonly', false)
-      $('#noshi_namae5').prop('readonly', false)
+      $('#noshi_namae4, #noshi_namae5').prop('readonly', false)
     };
     if ($('#noshi_ntype_16').is( ':checked' )) {
       $('#noshi_omotegaki').val('空白')
